53ef3e56a71c993297536eca2a2b393f25df12dc
Hilderico Pereira <hilderico.pereira@gmail.com>
2018-02-25 14:28:26 -0300

    atualização shutdown

--- table2.php
+++ table2.php
@@ -1,34 +1,66 @@
 <html>
 	<head>
+		<style>
+		table{
+			border-style: solid;
+			border-width: 1px;
+			border-radius: 5px;
+			background-color: green;
+			
+			
+		}
+
+		table tr:nth-child(even){
+			background-color: #f2f2f2;
+			
+		}
+
+		table tr:nth-child(odd) {
+    		background: red;
+		}
+
+		th{
+			background-color: green;
+		}
+
+		tr{
+			border-collapse: collapse;
+		}
+
+		</style>
+
 	</head>
 
 	<body>
-		<?php $COL1 = "Ola";		
+		<?php $COL1 = "Ola Ola Ola";		
 		$WID = strlen($COL1);
-		echo "<div style=\"background-color: #6c7ae0; padding:5px; width:" .$WID ."em; text-align: center; color: white;\">";
-		?>
-			<table>
-				<tr>
-					<th> OLA </th>
-<th> OLA </th>
-<th> OLA </th>
-				</tr>
-			</table>
-
-		</div>
-		
-
-		
-		
-		
-		<?php $COL1 = "Ola";
-		
-		$WID = strlen($COL1);
-		echo "<div style=\"background-color: #6c7ae0; padding:5px; width:" .$WID ."em; text-align: center; color: white;\">";
-		echo $COL1;
+		echo "<div style=\"background-color: #6c7ae0; padding:5px; width:" .$WID ."em; text-align: center; color: white; align: center;\">";
+		echo "" .$COL1;
 		echo "</div>";
 		?>
+
+		<br>
 		<br>
 
+		<table>
+		<tr>
+			<th> test </th>
+			<th> test </th>
+			<th> test </th>
+		</tr>	
+
+		<tr>
+			<td> test asfasf</td>
+			<td> test </td>
+			<td> test dsafasfasf</td>
+		</tr>	
+
+		<tr>
+			<td> test </td>
+			<td> test asfasf </td>
+			<td> test dsafasfasf</td>
+		</tr>	
+		</table>		
+		
 	</body>
 </html>
---
+++ test3.php
@@ -0,0 +1,17 @@
+<html>
+	<head>	
+	</head>
+	<body>	
+		<div style="width: 100px; background-color: green; color: white;">	
+			<table>
+				<thead>
+					<tr>
+						<th> COL1 </th>
+						<th> COL2 </th>
+						<th> COL3 </th>
+					</tr>
+				</thead>
+			</table>
+		</div>	
+	</body>
+</html>
