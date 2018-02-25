d4723831df118b549ed23b4ed297a9b282d7b292
Hilderico Pereira <hilderico.pereira@gmail.com>
2018-02-25 11:25:31 -0300

    Atualizando

---
+++ table2.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+	</head>
+
+	<body>
+		<?php $COL1 = "Ola";		
+		$WID = strlen($COL1);
+		echo "<div style=\"background-color: #6c7ae0; padding:5px; width:" .$WID ."em; text-align: center; color: white;\">";
+		?>
+			<table>
+				<tr>
+					<th> OLA </th>
+<th> OLA </th>
+<th> OLA </th>
+				</tr>
+			</table>
+
+		</div>
+		
+
+		
+		
+		
+		<?php $COL1 = "Ola";
+		
+		$WID = strlen($COL1);
+		echo "<div style=\"background-color: #6c7ae0; padding:5px; width:" .$WID ."em; text-align: center; color: white;\">";
+		echo $COL1;
+		echo "</div>";
+		?>
+		<br>
+
+	</body>
+</html>
